09f826b4dca43f750a35c14884d2b3f99194215e
Amit Mendapara <mendapara.amit@gmail.com>
2025-04-04 16:03:59 +0530

    Rework on dependency analyzer for better analysis

--- src/main/java/org/eclipse/birt/publisher/Analyzer.java
+++ src/main/java/org/eclipse/birt/publisher/Analyzer.java
@@ -1,33 +1,33 @@
 package org.eclipse.birt.publisher;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.spi.ToolProvider;
 import java.util.stream.Collectors;
 
-/** Analyze jar files and their dependencies. */
 public class Analyzer {
 
-  /** Find all jar files in the classpath. */
-  public List<Path> findJars() {
+  public static List<Path> findClassPath() {
     var classPath = System.getProperty("java.class.path");
-    var classList = List.of(classPath.split(System.getProperty("path.separator")));
-    return classList.stream().filter(path -> path.endsWith(".jar")).map(Path::of).toList();
+    var classList = Arrays.asList(classPath.split(File.pathSeparator));
+    return classList.stream()
+        .filter(path -> path.endsWith(".jar"))
+        .map(Path::of)
+        .filter(Files::exists)
+        .toList();
   }
 
-  /**
-   * Find dependencies of a jar file.
-   *
-   * <p>This method uses the jdeps tool to analyze the jar file and find its dependencies.
-   *
-   * @param jarPath the jar file to analyze
-   * @param classPath the classpath to use for analysis
-   * @return a list of jar files that the given jar file depends on
-   */
-  public List<Path> findDeps(Path jarPath, List<Path> classPath) {
+  private static List<Path> findDeps(Path jarPath, List<Path> classPath) {
     var tool = ToolProvider.findFirst("jdeps").orElseThrow();
     var out = new StringWriter();
     var err = new StringWriter();
@@ -38,11 +38,7 @@ public class Analyzer {
       jars.add(jarPath);
     }
 
-    var cp =
-        jars.stream()
-            .map(Path::toString)
-            .collect(Collectors.joining(System.getProperty("path.separator")));
-
+    var cp = jars.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
     tool.run(
         new PrintWriter(out),
         new PrintWriter(err),
@@ -64,15 +60,118 @@ public class Analyzer {
         .toList();
   }
 
-  /**
-   * Find dependencies of a jar file using the classpath.
-   *
-   * @param jarPath the jar file to analyze
-   * @return a list of jar files that the given jar file depends on
-   * @see #findDeps(Path, List)
-   */
-  public List<Path> findDeps(Path jarPath) {
-    var classPath = findJars();
-    return findDeps(jarPath, classPath);
+  public static List<JarLib> findLibs() {
+    var jars = findClassPath();
+    var libs = new LinkedHashMap<Path, JarLib>();
+
+    // Initialize libs with jars
+    for (var jar : jars) {
+      libs.put(jar, new JarLib(jar));
+    }
+
+    // Process jars in parallel
+    jars.parallelStream()
+        .forEach(
+            jar -> {
+              try {
+                var deps = findDeps(jar, jars);
+                var lib = libs.get(jar);
+                for (var dep : deps) {
+                  var depLib = libs.get(dep);
+                  if (depLib != null) {
+                    lib.dependsOn(depLib);
+                  }
+                }
+              } catch (Exception e) {
+                // Ignore errors
+              }
+            });
+
+    return libs.values().stream().toList();
+  }
+
+  private static String quote(String str) {
+    return "\"" + str + "\"";
+  }
+
+  private static void dots(Writer writer, List<JarLib> libs, Set<JarLib> visited) {
+    var out = new PrintWriter(writer);
+    for (var lib : libs) {
+      if (visited.contains(lib)) {
+        continue;
+      }
+      visited.add(lib);
+      out.println(String.format("  %s [label=%s];", quote(lib.name), quote(lib.name)));
+      for (var dep : lib.deps) {
+        out.println(String.format("  %s -> %s;", quote(lib.name), quote(dep.name)));
+        dots(writer, dep.deps, visited);
+      }
+    }
+  }
+
+  public static void dots(Writer writer, List<JarLib> libs) {
+    var out = new PrintWriter(writer);
+    out.println("digraph G {");
+    out.println("  splines=sfdp;");
+    out.println("  splines=ortho;");
+    out.println("  overlap=flase;");
+    out.println("  node[shape=box];");
+    dots(writer, libs, new HashSet<>());
+    out.println("}");
+  }
+
+  public static class JarLib {
+
+    public final Path jar;
+
+    public final String name;
+
+    public final List<JarLib> deps;
+
+    public JarLib(Path jar) {
+      this.jar = jar;
+      this.name = jar.getFileName().toString();
+      this.deps = new ArrayList<>();
+    }
+
+    public void dependsOn(JarLib dep) {
+      if (!deps.contains(dep)) {
+        deps.add(dep);
+      }
+    }
+
+    public void pprint(Writer writer) {
+      pprint(new PrintWriter(writer), 0, new HashSet<>());
+    }
+
+    private void pprint(PrintWriter out, int level, Set<JarLib> visited) {
+      var indent = "  ".repeat(level);
+      out.println(indent + name);
+
+      if (visited.contains(this)) {
+        return;
+      }
+
+      visited.add(this);
+
+      for (var dep : deps) {
+        dep.pprint(out, level + 1, visited);
+      }
+    }
+
+    @Override
+    public int hashCode() {
+      return jar.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      return obj == this || (obj instanceof JarLib other && jar.equals(other.jar));
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
   }
 }
--- src/test/java/org/eclipse/birt/publisher/DepsTest.java
+++ src/test/java/org/eclipse/birt/publisher/DepsTest.java
@@ -1,59 +1,37 @@
 package org.eclipse.birt.publisher;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 public class DepsTest {
 
-  private void print(Path jar, Map<Path, List<Path>> deps, int level, Set<Path> visited) {
-    if (visited.contains(jar)) {
-      return;
-    }
-    visited.add(jar);
-
-    var name = jar.getFileName().toString();
-    var indent = "  ".repeat(level);
-
-    System.out.println(indent + name);
-
-    var paths = deps.get(jar);
-    if (paths != null) {
-      for (var path : paths) {
-        print(path, deps, level + 1, visited);
-      }
-    }
-  }
-
-  private void print(Path jar, Map<Path, List<Path>> deps) {
-    print(jar, deps, 0, new HashSet<>());
-  }
-
   @Test
+  @Disabled
   public void test() {
-    var analyzer = new Analyzer();
-    var jars = analyzer.findJars();
-    var deps = new HashMap<Path, List<Path>>();
+    var libs = Analyzer.findLibs();
 
-    for (var jar : jars) {
-      try {
-        deps.put(jar, analyzer.findDeps(jar));
-      } catch (Exception e) {
-        // Ignore errors
-      }
+    // find birt libs
+    var birt = libs.stream().filter(lib -> lib.name.contains("birt")).toList();
+
+    // Generate graphviz dot file
+    try (var writer = Files.newBufferedWriter(Path.of("target", "deps.dot"))) {
+      Analyzer.dots(writer, birt);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
 
-    deps.forEach(
-        (jar, paths) -> {
-          if (paths != null) {
-            print(jar, deps);
-          }
-        });
+    // Generate tree view
+    try (var writer = Files.newBufferedWriter(Path.of("target", "deps.txt"))) {
+      for (var lib : birt) {
+        for (var dep : lib.deps) {
+          dep.pprint(writer);
+          writer.write("\n");
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
